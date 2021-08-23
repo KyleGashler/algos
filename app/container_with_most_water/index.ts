a8139adb0775be3c75dde5d8453bcca8f5b50b73
Kyle Gashler <kgashler@newrelic.com>
2021-08-23 12:47:35 -0600

    checking in

--- app/container_with_most_water/index.ts
+++ app/container_with_most_water/index.ts
@@ -2,7 +2,8 @@
  * @param {number[]} height
  * @return {number}
  */
-export const maxArea = (height: number[]) => {
+
+export const maxArea = (height: number[]): number => {
     let highestVolume: number = 0;
     let index1: number = 0;
     let index2: number = 0;
@@ -15,9 +16,8 @@ export const maxArea = (height: number[]) => {
         while (index2 <= height.length) {
             runComplex++;
             const newVolume = calcVolume(index1, height[index1], index2, height[index2]);
-            if (newVolume > highestVolume) {
-                highestVolume = newVolume;
-            }
+            newVolume > highestVolume ? (highestVolume = newVolume) : true;
+
             index2++;
         }
 
@@ -28,7 +28,7 @@ export const maxArea = (height: number[]) => {
     return highestVolume;
 };
 
-const calcVolume = (index1: number, val1: number, index2: number, val2: number) => {
+const calcVolume = (index1: number, val1: number, index2: number, val2: number): number => {
     const length = index2 - index1;
     const height = Math.min(val1, val2);
 
